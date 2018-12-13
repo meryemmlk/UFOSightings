8190009e367c1a01c9511fa83ec1d90c2057ac8a
meryemmlk <meryemkaratas@yahoo.com>
2018-12-12 23:38:04 -0500

    with bonus

--- static/js/app.js
+++ static/js/app.js
@@ -12,16 +12,46 @@ submit.on("click", function() {
   d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var inputElement1 = d3.select("#datetime");
+  var inputElement2 = d3.select("#city");
+    var inputElement3 = d3.select("#state");
+    var inputElement4 = d3.select("#country");
+    var inputElement5 = d3.select("#shape");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var inputDate = inputElement1.property("value");
+    var inputCity = inputElement2.property("value");
+    var inputState = inputElement3.property("value");
+    var inputCountry = inputElement4.property("value");
+    var inputShape = inputElement5.property("value");
 
-  console.log(inputValue);
+  console.log(inputDate);
+  console.log(inputCity);
+    
+    console.log(inputState);
+    console.log(inputCountry);
+    console.log(inputShape);
   console.log(tableData);
-
-  var filteredData = tableData.filter(person => person.datetime === inputValue);
-
+    
+  var filteredData = [];
+  var inputs = [inputDate,inputCity,inputState,inputCountry,inputShape];
+  var filters = ["datetime","city","state","country","shape"];
+  var nonBlankInputs = [];
+  var ourFilters = [];
+  for(var i =0; i<5; i++){
+      if (inputs[i] !== ""){
+          console.log(inputs[i])
+          nonBlankInputs.push(inputs[i]);
+          ourFilters.push(filters[i]);
+      }
+  };
+    
+  for(var i =0; i< nonBlankInputs.length; i++) {
+    filteredData = tableData.filter(row => row[ourFilters[i]] === nonBlankInputs[i]);
+  }
+      
+    
+    
   console.log(filteredData);
   d3.selectAll("#ufo-table>tbody>tr").remove();
   d3.selectAll("#ufo-table>tbody>td").remove();
